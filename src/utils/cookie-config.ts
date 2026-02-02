75f6b02f4a9363f34ccd48cddfecb1cfa5c418f0
udayhasan <udayhasan15@gmail.com>
2026-02-03 00:36:43 +0600

    cookie config

--- src/utils/cookie-config.ts
+++ src/utils/cookie-config.ts
@@ -4,6 +4,6 @@ import { config } from "../config/env";
 export const getCookieOptions = (): CookieOptions => ({
   httpOnly: true,
   secure: config.nodeEnv === "production",
-  sameSite: config.nodeEnv === "production" ? "none" : "lax",
+  sameSite: "none",
   path: "/",
 });
