54ef3b3fe7f45e71cf7c4e75abcbfdf646a91f64
udayhasan <udayhasan15@gmail.com>
2026-02-03 00:08:54 +0600

    vercel issue

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,20 +1,22 @@
+import { config } from "../config/env";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { config } from "../config/env";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-// Use a single pool instance
-const pool = new Pool({ connectionString: config.database.url });
+const pool = new Pool({
+  connectionString: config.database.url,
+});
+
 const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter, // This is now a stable feature
+    adapter,
     log:
       config.nodeEnv === "development" ? ["query", "warn", "error"] : ["error"],
   });
